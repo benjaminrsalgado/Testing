395235cc05c00534a59bf5dffc6eb01f8c29dced
Benjamin Rojo <benjaminrsalgado@gmail.com>
2025-09-30 16:58:39 -0600

    i practice forms, i did just simple one

---
+++ formPractice/formPractice/formPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  formPracticeApp.swift
+//  formPractice
+//
+//  Created by Benjamin Rojo on 30/09/25.
+//
+
+import SwiftUI
+
+@main
+struct formPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
