9feb932a5b91079917dfa0aa600485a5af05eb11
Benjamin Rojo <benjaminrsalgado@gmail.com>
2025-10-09 17:03:30 -0600

    agregue firebase, todo bien

--- news-app/src/environments/environment.ts
+++ news-app/src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+  apiKey: "AIzaSyAfBiisDNwmFsUYT238n4qq4Tgkpy-vH-U",
+  authDomain: "news-app-8a329.firebaseapp.com",
+  projectId: "news-app-8a329",
+  storageBucket: "news-app-8a329.firebasestorage.app",
+  messagingSenderId: "642477889789",
+  appId: "1:642477889789:web:371ad8b988026053da8694"
+}
 };
 
 /*
